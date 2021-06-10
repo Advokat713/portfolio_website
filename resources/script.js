fc9caf7f02b1052b6be9ec069ef5d518893281a2
Dmitry Nazarov <DmitriyAdvokatNazarow@yandex.ru>
2021-06-10 18:49:50 +0300

    Некоторые доработки

--- resources/script.js
+++ resources/script.js
@@ -1,12 +1,16 @@
-// Плавный скрол страницы до блока
-const links = document.querySelectorAll('a[href^="#"]');
-links.forEach(item => item.addEventListener('click',
- function(e) {
-	e.preventDefault();
-	const id = item.getAttribute('href').slice(1);
+// links = document.querySelectorAll('a[href^="#"]');
+// links.forEach(item => item.addEventListener('click',
+//  function(e) {
+// 	e.preventDefault();
+// 	let id = item.getAttribute('href').slice(1);
 
-	document.getElementById(id).scrollIntoView({
-		behavior: 'smooth',
-		block: 'start'
-	});
-}));
+//     let element = document.getElementById(id);
+//     let headerOffset = 58;
+//     let elementPosition = element.getBoundingClientRect().top;
+//     let offsetPosition = elementPosition - headerOffset;
+
+//     window.scrollTo({
+//         top: offsetPosition,
+//         behavior: "smooth"
+//     });
+// }));
