6f70b40bf38ea0425567873d4ba8d20e53d84e44
Dmitry Nazarov <DmitriyAdvokatNazarow@yandex.ru>
2021-07-08 22:39:28 +0300

    Переработал футер, добавил блок о себе и портфолио

--- resources/script.js
+++ resources/script.js
@@ -14,3 +14,29 @@
 //         behavior: "smooth"
 //     });
 // }));
+
+
+// * Функция для инициализации окна уведомления
+document.getElementById("ShowEmailInfo").onclick = function() {
+    var myAlert = document.getElementById('EmailInfo');
+    var bsAlert = new bootstrap.Toast(myAlert);
+    bsAlert.show();
+}
+
+
+// * Функция добавления текста в буфер обмена
+document.getElementById("CopyEmailBtn").onclick = function() {
+    navigator.clipboard.writeText('d.a.nazarov1@yandex.ru')
+    .then(()=>{
+        document.getElementById('CopySuccesses').style.display='block'
+        setTimeout(function(){
+            document.getElementById('CopySuccesses').style.display='none';
+        }, 2000);
+    })
+    .catch(err =>{
+        document.getElementById('CopyErrore').style.display='block'
+        setTimeout(function(){
+            document.getElementById('CopyErrore').style.display='none';
+        }, 2000);
+    });
+}
